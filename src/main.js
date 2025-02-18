2964829ffd806c7a67c5bf9c1184c78abcd81920
jluis-imac <skateluis27@hotmail.com>
2025-02-17 21:14:37 -0600

    feat: add vue-toastification

--- src/main.js
+++ src/main.js
@@ -5,9 +5,11 @@ import router from '@/router'
 import Vue3EasyDataTable from 'vue3-easy-data-table'
 import i18nConfig from '@/config/i18n'
 import { createI18n } from 'vue-i18n'
+import Toast from 'vue-toastification'
 import 'vue3-easy-data-table/dist/style.css'
 import '@/config/veevalidate'
 import 'bootstrap-icons/font/bootstrap-icons.css'
+import 'vue-toastification/dist/index.css'
 
 
 const i18n = createI18n(i18nConfig)
@@ -15,5 +17,6 @@ const i18n = createI18n(i18nConfig)
 const app = createApp(App)
 app.use(router)
 app.use(i18n)
+app.use(Toast)
 app.component('EasyDataTable', Vue3EasyDataTable)
 app.mount('#app')
