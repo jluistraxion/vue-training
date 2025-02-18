bbaf18697d18c5cffef499af008af32d8fa2ddbf
jluis-imac <skateluis27@hotmail.com>
2025-02-17 18:56:31 -0600

    feat: table column translations

--- src/modules/products/ProductsList.vue
+++ src/modules/products/ProductsList.vue
@@ -9,23 +9,29 @@
 </template>
 
 <script setup>
+import { computed } from 'vue'
 import { Form } from 'vee-validate'
+import { useI18n } from 'vue-i18n'
 import Input from '@/components/form/Input.vue'
 import Button from '@/components/buttons/Button.vue'
+
+const { t } = useI18n()
 
 const initialValues = { Name: '' }
 const schema = { Name: 'required' }
 
 const handleSubmit = (values) => console.log('handleSubmit', values)
 
-const headers = [
-  { text: 'Id', value: 'id' },
-  { text: 'Name', value: 'name' }
-]
+const headers = computed(() => [
+  { text: 'Id', value: 'id', sortable: true },
+  { text: t('Title'), value: 'title', sortable: true },
+  { text: t('Category'), value: 'category', sortable: true },
+  { text: t('Price'), value: 'price', sortable: true }
+])
 
 const items = [
-  { id: 1, name: 'tets row 1' },
-  { id: 2, name: 'test row 2' },
-  { id: 3, name: 'row 3' }
+  { id: 1, title: 'tets row 1', category: 'test category', price: 10.45 },
+  { id: 2, title: 'test row 2', category: 'test category', price: 5.25 },
+  { id: 3, title: 'test row 3', category: 'test category', price: 99.9 }
 ]
 </script>
